0a6b8df309d340dd18bf251a62f4d3f8694c5da5
fumi238000 <s21231128@gmail.com>
2022-06-27 20:27:41 +0900

    テストデータの実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,32 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# 2つのタイプのユーザーを作成する
+# マネージャー
+User.find_or_create_by!(name: "マネージャーユーザー") do |u|
+  u.name = "マネージャーユーザー"
+  u.position = 0
+  #   u.position =  "manager" # 確認する
+end
+
+# スタッフ
+User.find_or_create_by!(name: "スタッフユーザー") do |u|
+  u.name = "スタッフユーザー"
+  u.position = 1
+  #   u.position =  "staff" # 確認する
+end
+
+# 5つのグループを作成する
+GROUP_NUM = 5
+
+GROUP_NUM.times do |i|
+  Group.find_or_create_by!(name: "グループ#{i + 1}")
+end
+
+# ユーザーにグループを持たせる(表現)
+Group.all.each do |group|
+  # マネージャー
+  manager_user = User.find_by(name: "マネージャーユーザー")
+  GroupUser.find_or_create_by!(group_id: group.id, user_id: manager_user.id)
+
+  # スタッフ
+  staff_user = User.find_by(name: "スタッフユーザー")
+  GroupUser.find_or_create_by!(group_id: group.id, user_id: staff_user.id)
+end
