f4364d2fab9ce1becc02155e6b1f526f5fdbe473
fumi238000 <s21231128@gmail.com>
2022-06-28 07:37:00 +0900

    enumの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :group_users, dependent: :nullify
   has_many :groups, through: :group_users
+
+  # enum
+  enum position: {
+    manager: 0,
+    staff: 1,
+  }
 end
