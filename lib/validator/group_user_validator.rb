2d5363a6731a8ddd8998568df082b4e46c0374c7
fumi238000 <s21231128@gmail.com>
2022-06-28 08:43:08 +0900

    fix: rubocop -A

--- lib/validator/group_user_validator.rb
+++ lib/validator/group_user_validator.rb
@@ -2,8 +2,6 @@ class GroupUserValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     max_group_num = 1
 
-    if value.staff? && value.groups.count >= max_group_num
-      record.errors.add(attribute, "スタッフの場合、#{max_group_num}つまでしか管理できません。")
-    end
+    record.errors.add(attribute, "スタッフの場合、#{max_group_num}つまでしか管理できません。") if value.staff? && value.groups.count >= max_group_num
   end
 end
